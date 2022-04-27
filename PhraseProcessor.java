ae373ff9544747bdafd4dd2dfd30494e91c8fc31
MariDantas <61691909+MariDantas@users.noreply.github.com>
2022-04-26 22:36:30 -0300

    Add files via upload

--- PhraseProcessor.java
+++ PhraseProcessor.java
@@ -4,11 +4,16 @@ import java.util.Scanner;
 public class PhraseProcessor {
 	public static void main(String[] args) {
 		ArrayList<String> phrases = new ArrayList<>();
+		String teste[];
 		Scanner readPhrase = new Scanner(System.in);
 		Scanner readOp = new Scanner(System.in);
 		int option;
 		String phrase;
 		boolean cont = false;
+		String snip;
+		Scanner readSnip = new Scanner(System.in);
+		ArrayList<String> foundSnip = new ArrayList<>();
+		boolean found;
 		
 		System.out.println("Enter a phrase: ");
 		phrase = readPhrase.nextLine();
@@ -45,10 +50,6 @@ public class PhraseProcessor {
 							}
 							System.out.println("");
 						} else if (option == 2) {
-							String snip;
-							Scanner readSnip = new Scanner(System.in);
-							ArrayList<String> foundSnip = new ArrayList<>();
-							boolean found;
 							System.out.println("Enter the snippet you want to search for: ");
 							snip = readSnip.nextLine();
 							System.out.println("");
@@ -72,7 +73,7 @@ public class PhraseProcessor {
 									System.out.println(foundSnip.get(i));
 								}
 							} else {
-								System.out.println("No phrase found");
+								System.out.println("No phrases found");
 							}
 						}
 						System.out.println("Choose (enter a number corresponding to the desired option): ");
@@ -92,6 +93,8 @@ public class PhraseProcessor {
 				System.out.println("Error: the option entered is not a number!");
 			}
 			
+		} else {
+			System.out.println("No phrases were entered. End of program");
 		}
 		
 		
